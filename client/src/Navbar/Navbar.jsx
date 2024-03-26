21c32fc4c2d381a05a1f99ceafb88dafb53cf459
roquebernedo <roquebernedo@gmail.com>
2024-03-26 14:58:39 -0500

    fixing

--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -95,7 +95,7 @@ const Navbar = ({ filter, setFilter }) => {
         </div>
         { userInfo ? (
           <div className='log-reg'>
-            <Link onClick={() =>  setTimeout(recargarPagina, 1000)} className='sign profile' to='/profile'>
+            <Link className='sign profile' to='/profile'>
               <svg className='svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="18" role="presentation" alt="" data-testid="UserIcon" size="18" color="currentColor"><path d="M16.22 19.41A9.71 9.71 0 1 1 26 9.7a9.74 9.74 0 0 1-9.8 9.71M1.84 32a10.88 10.88 0 0 1 10.94-10.74h6.57A10.88 10.88 0 0 1 30.29 32H1.84" fill="currentColor"></path></svg>
               <div className='userinfo'>{userInfo.name}</div>
             </Link>
@@ -113,7 +113,7 @@ const Navbar = ({ filter, setFilter }) => {
           </div>
         ) : (
           <div className='log-reg'>
-            <Link onClick={() =>  setTimeout(recargarPagina, 100)} className='sign in' to='/login'>
+            <Link className='sign in' to='/login'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="18" role="presentation" alt="" data-testid="UserIcon" size="18" color="currentColor"><path d="M16.22 19.41A9.71 9.71 0 1 1 26 9.7a9.74 9.74 0 0 1-9.8 9.71M1.84 32a10.88 10.88 0 0 1 10.94-10.74h6.57A10.88 10.88 0 0 1 30.29 32H1.84" fill="currentColor"></path></svg>
               <div>Iniciar sesion / Registrate</div>
             </Link>
