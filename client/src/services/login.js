b57f56862658bd1dd6679140cc2a02dedaa347f7
Lillian Bernedo <roquebernedo@gmail.com>
2024-04-25 15:38:53 -0500

    server changed

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'https://ecommerce-moez.onrender.com/api/users/auth' //https://ecommerce-moez.onrender.com
+const baseUrl = 'https://e-commerce-f1fr.onrender.com/api/users/auth' //https://ecommerce-moez.onrender.com
 //http://localhost:8000
+//https://e-commerce-f1fr.onrender.com
 // const login = async credentials => {
 //   const response = await axios.post(baseUrl, credentials)
 //   return response.data
