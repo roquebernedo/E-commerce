05a8def426d46a44ea8bdb41ba06ae8b1cbeea81
Lillian Bernedo <roquebernedo@gmail.com>
2024-04-24 21:03:35 -0500

    fixed

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:8000' }) //'https://ecommerce-moez.onrender.com'
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://ecommerce-moez.onrender.com' }) //'https://ecommerce-moez.onrender.com'
 //http://localhost:8000
 export const apiSlice = createApi({
     baseQuery,
