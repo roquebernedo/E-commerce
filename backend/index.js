6b3b1b35958c25ad0317dd847e83b8a9f2092cbd
Lillian Bernedo <roquebernedo@gmail.com>
2024-04-24 20:48:33 -0500

    fixing.

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,22 @@ connectDB()
 const app = express()
 
 app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'https://main--ecommerce-rq.netlify.app/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 app.use(tokenExtractor)
 app.use("/api/stripe", stripeRoutes)
