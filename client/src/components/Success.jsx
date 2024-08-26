d36f85c2321782c5ae38b35e2f0522351feb3ff8
unknown <roquebernedo@gmail.com>
2024-08-26 12:45:12 -0500

    problem fixed in production

--- client/src/components/Success.jsx
+++ client/src/components/Success.jsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { cleanCart } from '../slices/authSlice'
 import '../styles/Success.scss'
 import { useNavigate } from 'react-router-dom'
 
 const Success = () => {
-  const [loading, setLoading] = useState(true);
+  //const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   //const { userInfo } = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -27,35 +27,25 @@ const Success = () => {
 //     return () => clearTimeout(timer);
 // }, [dispatch, navigate]);
 
-const update = useCallback(async () => {
-  console.log("entro al update de success");
-  await dispatch(cleanCart());
-  setLoading(false);
-}, [dispatch]);  // Aquí solo depende de dispatch
-
-// if(userInfo){
-//   update()
-// }
-
-useEffect(() => {
-  update();
-  console.log("bota tu gaaa");
-
-  const timer = setTimeout(() => {
-      console.log("estamos en el settimout");
-      navigate('/');
-  }, 5000);
-
-  return () => clearTimeout(timer);
-}, [update, navigate]);
+const update = () => {
+  console.log("entro al update de success")
+  dispatch(cleanCart())
+  console.log("ojala entre ctmre")
+  navigate('/');
+  //setLoading(false);
+};
 
   return (
     <div className='mainSuccess'>
-      {loading ? (
+      {/* {loading ? (
         <div className="loading">Procesando su pedido, por favor espere...</div>
       ) : (
-        <div className='gracias'>¡Gracias por su compra!</div>
-      )}
+        <> */}
+          <div className='gracias'>¡Gracias por su compra!</div>
+          <div>Presione continuar para seguir con otra compra!</div>
+          <button onClick={() => update()}>Continuar</button>
+        {/* </>
+      )} */}
       {/* <button onClick={() => update()}>Gracias por su compra!</button> */}
     </div>
   );
